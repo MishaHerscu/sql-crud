1587dcfbd6f8554ce4f9a51336b15ac18afd5397
MishaHerscu <mishaherscu@gmail.com>
2016-06-14 11:40:57 -0400

    fixed  null value by adding  empty string

--- scripts/insert_into/cities.sql
+++ scripts/insert_into/cities.sql
@@ -3,4 +3,4 @@ INSERT INTO cities
 (population, name, country, region, longitude, latitude)
 VALUES
 (210700,'Aberdeen','UK','Scotland',-2.083,57.150),
-(1950000,'Abidjan','Ivory Coast',,-3.867,5.333);
+(1950000,'Abidjan','Ivory Coast',"",-3.867,5.333);
